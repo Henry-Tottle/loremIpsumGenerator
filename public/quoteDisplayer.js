fc80cedf8c23679bcdf0f24608c2e909cfda936a
Henry Tottle <henrywilliamtottle@gmail.com>
2024-10-04 12:18:38 +0100

    only displays the amout of paragraphs that you ask fo

--- public/quoteDisplayer.js
+++ public/quoteDisplayer.js
@@ -1,7 +1,10 @@
+
+
+
 const eventButton = document.getElementById('eventButton')
 
 const x = document.querySelector('#paragraphs')
-
+const ipsumDiv = document.getElementById('ipsumDisplay')
 
 
 const result = fetch("http://localhost:3023/quotes").then((response) => {
@@ -13,26 +16,45 @@ const result = fetch("http://localhost:3023/quotes").then((response) => {
 
     const quotes = data.quotes
 
-    eventButton.addEventListener('click', (event) => {
-        let sentence = ''
+    function shuffleArray(array) {
+        for (let i = array.length - 1; i >= 0; i--) {
+            let j = Math.floor(Math.random() * (i + 1));
+            let temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
 
+
+
+
+    eventButton.addEventListener('click', (event) => {
+        let child = ipsumDiv.lastElementChild
+        while (child) {
+            ipsumDiv.removeChild(child)
+            child = ipsumDiv.lastElementChild
+        }
+        for (let count=1; count<=parseInt(x.value); count++) {
+            shuffleArray(quotes)
+            let sentence = ''
         quotes.forEach(function(quote) {
             sentence = sentence + quote + ' '
         })
 
         sentence = sentence.substring(0,sentence.length -1) + '.'
-        console.log(sentence)
-        const ipsumDiv = document.getElementById('ipsumDisplay')
         const ipsumParagraph = document.createElement('p')
+
         ipsumParagraph.innerText = sentence
         ipsumDiv.appendChild(ipsumParagraph)
         console.log(parseInt(x.value))
         event.preventDefault()
+        }
     })
 
 
-
 })
+
+
 
 
 
